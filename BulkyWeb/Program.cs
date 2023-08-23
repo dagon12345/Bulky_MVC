826defacdfe8e372803ad1717adc8e5b878f7573
dagon12345 <lanceespina478@gmail.com>
2023-08-23 08:41:54 +0800

    updates

--- BulkyWeb/Program.cs
+++ BulkyWeb/Program.cs
@@ -1,4 +1,4 @@
-using BulkyWeb.Data;
+using Bulky.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options=>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));//////////Added and created..
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));//////////Added and created..
 
 
 
